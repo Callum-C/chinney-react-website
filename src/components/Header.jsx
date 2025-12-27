6d64a917215b76793a52ca1a01833338d361ac5e
Callum-C <60474698+Callum-C@users.noreply.github.com>
2025-12-27 13:45:29 +0000

    Header inline styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import { Trophy } from 'lucide-react'
 export default function renderHeader() {
 
   return (
-    <header className='app-header'>
+    <header className='w-full shadow-lg mb-8'>
 
-      <div className='header-brand-row'>
-        <div className='header-brand-container'>
+      <div className='w-full bg-chinney-purple border-b-2 border-chinney-gold'>
+        <div className='max-w-7xl mx-auto p-4 flex items-center justify-center gap-3'>
           <div className='p-2 rounded-lg'>
             <Trophy size={20} className='text-chinney-gold'/>
           </div>
@@ -16,10 +16,10 @@ export default function renderHeader() {
       </div>
       
 
-      <div className='header-nav-row'>
-        <div className='header-nav-container'>
+      <div className='w-full bg-slate-800'>
+        <div className='max-w-7xl mx-auto px-4 py-3'>
           <nav>
-            <ul className='header-nav-bar'>
+            <ul className='bg-slate-800 flex justify-center gap-8 text-sm font-medium text-slate-100'>
               <li>
                 <a className='header-nav-link' href =''>Player Lookup</a>
               </li>
