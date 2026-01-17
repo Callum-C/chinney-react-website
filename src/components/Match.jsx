60bc1049d72ae987d08953e55c3540ac0cb70b7f
Callum-C <60474698+Callum-C@users.noreply.github.com>
2026-01-17 15:59:29 +0000

    Match Mobile Adjustments

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -9,20 +9,27 @@ export default function renderMatch({ match, players }) {
     <div className="bg-slate-900 rounded-lg border border-slate-800 p-4 hover:border-slate-700 transition-all shadow-lg mb-4">
       
       {/*Match Header - Show Match ID, Date, Time, Duration and Winner*/}
-      <div className="flex flex-col md:flex-row justify-between items-center mb-4">
+      <div className="flex flex-row justify-between items-center mb-4">
 
         {/*Match Info - ID, Date, Duration*/}
-        <div className="flex items-center mb-2 md:mb-0 text-xs text-slate-500">
-          <span className='px-2 py-0.5 rounded text-xs font-bold bg-slate-800 text-slate-400 mr-2'>#{match.matchID}</span>
-          <span className=' flex items-center mr-4'>
-            <Calendar size={12} className='mr-1'/> {formatDate(match.createdAt)}
+        <div className="flex items-center mb-2 md:mb-0 text-xs text-slate-500 overflow-hidden">
+
+          <span className='px-2 py-0.5 rounded text-xs font-bold bg-slate-800 text-slate-400 mr-2'>
+            Match #{match.matchID}
           </span>
+
           <span className='flex items-center mr-4'>
-            <Clock size={12} className='mr-1'/> {formatTime(match.createdAt)}
+            <Calendar size={12} className='mr-1.5 opacity-70'/> {formatDate(match.createdAt)}
           </span>
+
           <span className='flex items-center mr-4'>
-            <Timer size={12} className='mr-1'/> {formatDuration(match.createdAt, match.updatedAt)}
+            <Clock size={12} className='mr-1.5 opacity-70'/> {formatTime(match.createdAt)}
           </span>
+
+          <span className='hidden sm:flex items-center mr-4'>
+            <Timer size={12} className='mr-1.5 opacity-70'/> {formatDuration(match.createdAt, match.updatedAt)}
+          </span>
+
         </div>
 
         {/*Match Winner in Green Text and Backing*/}
@@ -37,7 +44,7 @@ export default function renderMatch({ match, players }) {
         {/*Team 1 / Blue Team - Team Card*/}
         <div className={`flex-1 p-4 ${winner === 'Team 1' ? 'bg-blue-500/10' : 'bg-slate-900'}`}>
           <div className='flex justify-between items-center mb-3'>
-            <div className='text-blue-400 font-bold uppercase tracking-widest text-sm'>Blue Team</div>
+            <div className='text-blue-400 font-bold uppercase tracking-widest text-xs sm:text-sm whitespace-nowrap'>Blue Team</div>
             <span className={`text-2xl font-black ${winner === 'Team 1' ? 'text-white' : 'text-slate-600'}`}>{match.team1Score}</span>
           </div>
           <div>
@@ -57,7 +64,9 @@ export default function renderMatch({ match, players }) {
         <div className={`flex-1 p-4 ${winner === 'Team 2' ? 'bg-orange-500/10' : 'bg-slate-900'}`}>
           <div className='flex justify-between items-center mb-3'>
             <span className={`text-2xl font-black ${winner === 'Team 2' ? 'text-white' : 'text-slate-600'}`}>{match.team2Score}</span>
-            <div className="text-orange-400 font-bold uppercase tracking-widest text-sm">Orange Team</div>
+            <div className="text-orange-400 font-bold uppercase tracking-widest text-xs sm:text-sm whitespace-nowrap text-right">
+              Orange Team
+            </div>
           </div>
           <div>
             {match.team2.map(pid => (
